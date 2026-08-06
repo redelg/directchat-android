81af3c690aeab0edd12539b694da6a6d51c74801
Renzo Delgado <renzodelgado114@gmail.com>
2026-08-05 22:12:23 -0500

    Bump to versionCode 6 / 1.1.3 (versionCode 5 already released)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.codergang.chatdirecto"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
